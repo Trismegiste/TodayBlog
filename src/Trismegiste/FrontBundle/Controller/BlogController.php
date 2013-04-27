d8e34bc1a13200b9491cf1bc0ce9474fe586aa58
Trismegiste <florent.g@allopneus.com>
2013-04-27 23:26:57 +0200

    proof of concept (dirty coded)

--- src/Trismegiste/FrontBundle/Controller/BlogController.php
+++ src/Trismegiste/FrontBundle/Controller/BlogController.php
@@ -12,6 +12,12 @@ use Symfony\Component\HttpFoundation\Request;
 class BlogController extends Template
 {
 
+    protected function getSubdomain()
+    {
+        preg_match('#^([a-z0-9]+)\.#', $this->getRequest()->getHost(), $match);
+        return $match[1];
+    }
+
     public function aboutAction()
     {
         return $this->render('TrismegisteFrontBundle:Blog:about.html.twig');
@@ -19,13 +25,47 @@ class BlogController extends Template
 
     public function indexAction()
     {
+        $filter = $this->getSubdomain();
         $blog = $this
                 ->get('dokudoki.collection')
-                ->find(array('-class' => 'article'));
+                ->find(array(
+            '-class' => 'article',
+            'subdomain' => $filter
+        ));
         $blog->sort(array('publishedAt' => -1));
-        $article = $this->createForm(new \Trismegiste\FrontBundle\Form\ArticleType());
 
-        return $this->render('TrismegisteFrontBundle:Blog:write.html.twig', array('blog' => $blog, 'form' => $article->createView()));
+        $formType = new \Trismegiste\FrontBundle\Form\ArticleType();
+        $obj = null;
+        $target = $this->generateUrl('trismegiste_homepage');
+
+        $request = $this->getRequest();
+
+        if ($request->query->has('id')) {
+            $pk = $request->query->get('id');
+            $obj = $this->get('dokudoki.repository')->findByPk($pk);
+            $target = $this->generateUrl('trismegiste_homepage', array('id' => $pk));
+        }
+
+        $article = $this->createForm($formType, $obj);
+
+        if ($request->getMethod() == 'POST') {
+            $article->bind($request);
+            if ($article->isValid()) {
+                $publish = $article->getData();
+                $publish->setPublishedAt(time());
+                $publish->setSubdomain($this->getSubdomain());
+                $this->get('dokudoki.repository')->persist($publish);
+                $this->get('session')->getFlashBag()->add('notice', 'Saved');
+
+                return $this->redirect($this->generateUrl('trismegiste_homepage'));
+            }
+        }
+
+        return $this->render('TrismegisteFrontBundle:Blog:write.html.twig', array(
+                    'blog' => $blog,
+                    'form' => $article->createView(),
+                    'target' => $target
+        ));
     }
 
     protected function getTopMenu()
@@ -33,18 +73,12 @@ class BlogController extends Template
         return array('About' => 'trismegiste_about');
     }
 
-    public function editAction(Request $request)
+    public function removeAction($id)
     {
-        if ($request->getMethod() == 'POST') {
-            $article = $this->createForm(new \Trismegiste\FrontBundle\Form\ArticleType());
-            $article->bind($request);
-            if ($article->isValid()) {
-                $publish = $article->getData();
-                $publish->setPublishedAt(time());
-                $this->get('dokudoki.repository')->persist($publish);
-            }
-        }
-        return $this->indexAction();
+        $this->get('dokudoki.collection')->remove(array('_id' => new \MongoId($id)));
+        $this->get('session')->getFlashBag()->add('notice', 'Removed');
+
+        return $this->redirect($this->generateUrl('trismegiste_homepage'));
     }
 
 }
