19aa2f25a190adef9fe593dccfae8cad9eeaba12
Trismegiste <florent.g@allopneus.com>
2013-04-27 15:28:08 +0200

    with magic

--- src/Trismegiste/FrontBundle/Controller/BlogController.php
+++ src/Trismegiste/FrontBundle/Controller/BlogController.php
@@ -2,6 +2,8 @@
 
 namespace Trismegiste\FrontBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  * Example a simple controller with an About page
  *
@@ -19,12 +21,25 @@ class BlogController extends Template
     {
         $blog = array(array('title' => 'Ze title', 'body' => 'lorem ipsum'));
         $article = $this->createForm(new \Trismegiste\FrontBundle\Form\ArticleType());
+
         return $this->render('TrismegisteFrontBundle:Blog:write.html.twig', array('blog' => $blog, 'form' => $article->createView()));
     }
 
     protected function getTopMenu()
     {
         return array('About' => 'trismegiste_about');
+    }
+
+    public function editAction(Request $request)
+    {
+        if ($request->getMethod() == 'POST') {
+            $article = $this->createForm(new \Trismegiste\FrontBundle\Form\ArticleType());
+            $article->bind($request);
+            if ($article->isValid()) {
+                print_r($article->getData());
+            }
+        }
+        return $this->indexAction();
     }
 
 }
